97cac58a9b390a5a6c8a4cd0af99e171f73e31f5
hamza-m-masood <hamzamasood183@gmail.com>
2024-07-17 00:08:41 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: config.site.base_url ? config.site.base_url : "https://hamza-masood.com/",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: {
